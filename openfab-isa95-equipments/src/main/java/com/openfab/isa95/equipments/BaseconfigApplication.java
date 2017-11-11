eb0eb56889f4a1a7172c6a6584cf72e7ca0a642e
skioppetto <skioppetto@HOME-VAIO>
2017-11-11 23:37:30 +0100

    creato repository e progetto equipments #1

---
+++ openfab-isa95-equipments/src/main/java/com/openfab/isa95/equipments/BaseconfigApplication.java
@@ -0,0 +1,12 @@
+package com.openfab.isa95.equipments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseconfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaseconfigApplication.class, args);
+	}
+}
---
+++ openfab-isa95-equipments/src/test/java/com/openfab/isa95/equipments/BaseconfigApplicationTests.java
@@ -0,0 +1,16 @@
+package com.openfab.isa95.equipments;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BaseconfigApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
