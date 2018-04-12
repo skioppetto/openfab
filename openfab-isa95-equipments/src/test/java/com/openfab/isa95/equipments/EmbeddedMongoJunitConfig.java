8e3792feeb5b81967fceb0752e5863d8c6dfdab5
Alberto Coppini <alberto.coppini@valbruna.it>
2018-04-12 12:29:44 +0200

    Fixes #8 add junit config to test

---
+++ openfab-isa95-equipments/src/test/java/com/openfab/isa95/equipments/EmbeddedMongoJunitConfig.java
@@ -0,0 +1,75 @@
+package com.openfab.isa95.equipments;
+
+import java.io.IOException;
+
+import javax.annotation.PreDestroy;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDbFactory;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
+
+import com.mongodb.MongoClient;
+
+import de.flapdoodle.embed.mongo.MongodExecutable;
+import de.flapdoodle.embed.mongo.MongodProcess;
+import de.flapdoodle.embed.mongo.MongodStarter;
+import de.flapdoodle.embed.mongo.config.MongodConfigBuilder;
+import de.flapdoodle.embed.mongo.config.Net;
+import de.flapdoodle.embed.mongo.distribution.Version;
+import de.flapdoodle.embed.process.runtime.Network;
+
+@Configuration
+public class EmbeddedMongoJunitConfig {
+
+	private static final String DB_NAME = "junit";
+	
+	/**
+	 * please store Starter or RuntimeConfig in a static final field if you want
+	 * to use artifact store caching (or else disable caching)
+	 */
+	private static final MongodStarter starter = MongodStarter
+			.getDefaultInstance();
+
+	public MongodExecutable _mongodExe;
+	private MongodProcess _mongod;
+
+	private MongoClient _mongo;
+
+	public EmbeddedMongoJunitConfig() throws Exception {
+		_mongodExe = starter.prepare(new MongodConfigBuilder()
+				.version(Version.Main.V3_6)
+				.net(new Net("localhost", 12345, Network.localhostIsIPv6()))
+				.build());
+		_mongod = _mongodExe.start();
+
+		_mongo = new MongoClient("localhost", 12345);
+
+	}
+
+	@Bean(name = "mongoClient")
+	public MongoClient mongoClient() throws IOException {
+		// Lots of calls here to de.flapdoodle.embed.mongo code base to
+		// create an embedded db and insert some JSON data
+		return _mongo;
+	}
+
+	@Autowired
+	@Bean(name = "mongoDbFactory")
+	public MongoDbFactory mongoDbFactory(MongoClient mongoClient) {
+		return new SimpleMongoDbFactory(mongoClient, DB_NAME);
+	}
+
+	@Autowired
+	@Bean(name = "mongoTemplate")
+	public MongoTemplate mongoTemplate(MongoClient mongoClient) {
+		return new MongoTemplate(mongoClient, DB_NAME);
+	}
+
+	@PreDestroy
+	public void shutdownEmbeddedMongoDB() {
+		_mongodExe.stop();
+	}
+}
--- openfab-isa95-equipments/src/test/java/com/openfab/isa95/equipments/Isa95EquipmentsDbTests.java
+++ openfab-isa95-equipments/src/test/java/com/openfab/isa95/equipments/Isa95EquipmentsDbTests.java
@@ -1,16 +1,31 @@
 package com.openfab.isa95.equipments;
 
+import org.bson.BsonDocument;
+import org.bson.BsonString;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class Isa95EquipmentsDbTests extends AbstractMongoDBTest{
+import com.mongodb.MongoClient;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = { EmbeddedMongoJunitConfig.class,
+		Isa95EquipmentsApplication.class })
+public class Isa95EquipmentsDbTests {
+
+	@Autowired
+	private MongoClient mongoClient;
+	
 	@Test
 	public void contextLoads() {
+		String version = mongoClient.getDatabase("junit")
+				.runCommand(new BsonDocument("buildinfo", new BsonString("")))
+				.get("version").toString();
+		Assert.assertEquals("3.6.2", version);
+		
 	}
 
 }
