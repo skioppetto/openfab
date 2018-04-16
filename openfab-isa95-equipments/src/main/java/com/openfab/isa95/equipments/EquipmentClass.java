f1608931fc0699e7bd4cc4cb0974adfaf5f79010
Alberto Coppini <alberto.coppini@valbruna.it>
2018-04-16 13:50:02 +0200

    #24 add querydsl to manage query fields

--- openfab-isa95-equipments/src/main/java/com/openfab/isa95/equipments/EquipmentClass.java
+++ openfab-isa95-equipments/src/main/java/com/openfab/isa95/equipments/EquipmentClass.java
@@ -1,8 +1,11 @@
 package com.openfab.isa95.equipments;
 
+import org.springframework.data.mongodb.core.mapping.Document;
+
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
+@Document
 @Data
 @EqualsAndHashCode(callSuper=true)
 public class EquipmentClass extends AbstractEquipment{
--- openfab-isa95-equipments/src/main/java/com/openfab/isa95/equipments/EquipmentClassRepository.java
+++ openfab-isa95-equipments/src/main/java/com/openfab/isa95/equipments/EquipmentClassRepository.java
@@ -1,8 +1,13 @@
 package com.openfab.isa95.equipments;
 
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.repository.CrudRepository;
 
 public interface EquipmentClassRepository extends
-		CrudRepository<EquipmentClass, String>, EquipmentClassRepositoryCustom {
+		CrudRepository<EquipmentClass, String>, 
+		QuerydslPredicateExecutor<EquipmentClass>,
+		EquipmentClassRepositoryCustom {
 
+	
+	
 }
